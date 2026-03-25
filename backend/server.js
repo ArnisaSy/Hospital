51d508e14532da5c545d4e9ed6e4df5f0ca4b7e2
ArnisaSy <162451139+ArnisaSy@users.noreply.github.com>
2026-03-25 14:59:06 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,70 @@
 const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
 const app = express();
-const port = 3000;
+app.use(cors());
 
-app.use(express.json());
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'hospital_db'
+});
 
-// TEST ROUTE
+db.connect(err => {
+    if (err) {
+        console.log('DB ERROR:', err);
+    } else {
+        console.log('Connected to DB');
+    }
+});
+
+// ROUTES
+
 app.get('/', (req, res) => {
     res.send('Hospital API is running...');
 });
 
-// SAMPLE ROUTE
 app.get('/patients', (req, res) => {
-    res.json([
-        { id: 1, name: "Ana Krasniqi" },
-        { id: 2, name: "Blerim Gashi" }
-    ]);
+    db.query('SELECT * FROM Patient', (err, results) => {
+        if (err) return res.send(err);
+        res.json(results);
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.get('/top-doctors', (req, res) => {
+    db.query(`
+        SELECT d.Name, COUNT(*) AS TotalVisits
+        FROM Doctor d
+        JOIN Appointment a ON d.DoctorID = a.DoctorID
+        GROUP BY d.Name
+        ORDER BY TotalVisits DESC
+        LIMIT 3
+    `, (err, results) => {
+        if (err) return res.send(err);
+        res.json(results);
+    });
+});
+
+app.get('/stats', (req, res) => {
+    const stats = {};
+
+    db.query('SELECT COUNT(*) AS totalPatients FROM Patient', (err, result) => {
+        stats.patients = result[0].totalPatients;
+
+        db.query('SELECT COUNT(*) AS totalDoctors FROM Doctor', (err, result) => {
+            stats.doctors = result[0].totalDoctors;
+
+            db.query('SELECT COUNT(*) AS totalAppointments FROM Appointment', (err, result) => {
+                stats.appointments = result[0].totalAppointments;
+
+                res.json(stats);
+            });
+        });
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
